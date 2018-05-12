d25b92a77c3381f99c9f8b81d9c8e2258b26db25
Alex <2604161467@qq.com>
2018-05-13 04:26:30 +0800

    UNITY client sample code

---
+++ unity/Sample/Assets/Scenes/LoginScene.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using APIConnection;
+
+public class LoginScene : MonoBehaviour {
+
+    public InputField login_name;
+    public InputField login_passwd;
+    public Text system_message;
+
+	// Use this for initializatio n
+	void Start () {
+        APIConn.Inst.wsUri = "ws://112.124.70.60:51727/demo";
+        APIConn.Inst.logger = Debug.Log;
+        APIConn.Inst.response_received_handlers += this.response_hander;
+        APIConn.Inst.state_changed_handlers += this.state_hander;
+        APIConn.Inst.connect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        APIConn.Inst.Update();
+	}
+
+    public void LoginButtonClicked() {
+        Debug.Log("LoginButtonClicked:"+login_name.text);
+        APIConn.Inst.login(login_name.text, login_passwd.text);
+    }
+
+    public void ClickTestClicked() {
+        Debug.Log("ClickTestClicked");
+        JSONObject cmd = new JSONObject();
+        cmd.xput("obj", "test");
+        cmd.xput("act", "click");
+        cmd.xput("data", "anything");
+        APIConn.Inst.send_obj(cmd);
+    }
+
+    public void response_hander(JSONObject jo) {
+        if (jo.s("obj") == "test" && jo.s("act") == "click")
+            system_message.text = "server response: "+APIConn.Inst.getUnixTime();
+    }
+
+    public void state_hander(JSONObject jo)
+    {
+        if (jo.s("conn_state") == "IN_SESSION") SceneManager.LoadScene("MainScene");
+        //if (jo.s("conn_state") == "LOGIN_SCREEN_ENABLED") SceneManager.LoadScene("LoginScene");
+    }
+}
---
+++ unity/Sample/Assets/Scenes/MainScene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using APIConnection;
+
+public class MainScene : MonoBehaviour {
+
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+        APIConn.Inst.response_received_handlers += this.response_handler;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        APIConn.Inst.Update();
+	}
+
+    public void response_handler (JSONObject jo) {
+        
+        Debug.Log("mainscene response_handler");
+
+        if (jo.s("obj") == "test" && jo.s("act") == "scale")
+        {
+            if (jo.i("scale") == 0)
+            {
+                cube.gameObject.transform.localScale -= new Vector3((float)0.1, (float)0.1, (float)0.1);
+            }
+            else
+            {
+                cube.gameObject.transform.localScale -= new Vector3(jo.i("scale"), jo.i("scale"), jo.i("scale"));
+            }
+        }
+    }
+
+    public void ButtonClicked () {
+        JSONObject cmd = new JSONObject();
+        cmd.xput("obj", "test");
+        cmd.xput("act", "scale");
+        APIConn.Inst.send_obj(cmd);
+    }
+}
