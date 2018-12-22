afd7ad5dd391f0779cb549f375b8f91bec5c289b
Alex <2604161467@qq.com>
2018-12-22 12:49:15 +0800

    WeChat mini program SDK

---
+++ webapp-mobile-wechat/util.js
@@ -0,0 +1,42 @@
+import apimod from "APIConnectionWX.min.js";
+
+var apiconn = new apimod.APIConnection();
+apiconn.wsUri = "wss://wahalife.cn/waha_ga";
+
+apiconn.state_changed_handler = function() {
+};
+
+// callbacks to handle data reponse from server
+var cb = {};
+
+apiconn.response_received_handler = function(jo) {
+  if (jo.obj == "server" && jo.act == "info") {
+    apiconn.send_obj({
+	  obj: "loginInfo",
+	  act: "get",
+	  code: apiconn.user_data.wxcode
+	});
+  } else if (jo.obj == "loginInfo" && jo.act == "get") {
+    apiconn.loginx({
+	  access_token: jo.response.session_key,
+	  ctype: "user",
+	  openid: jo.response.openid
+	});
+  } else if (cb[jo.obj + "/" + jo.act]) {
+    cb[jo.obj + "/" + jo.act](jo);
+  } else {
+  	console.log("handler missing: "+jo.obj + "/" + jo.act);
+  }
+};
+
+function login_at_launch() {
+  wx.login({
+    success: res => {
+	  apiconn.user_data.wxcode = res.code;
+	  apiconn.connect();
+	}
+  });
+}
+
+// cb["a/b"] = function () {};
+// apiconn.send_obj({"obj":"a","act":"b",...});
